583fd0da9ad6240124cdbbddaa31e431d42fe618
Jagadish jena <bablujena2003@gmail.com>
2023-03-18 16:23:34 +0530

    attempt1

--- src/main/java/com/driver/repository/DriverRepository.java
+++ src/main/java/com/driver/repository/DriverRepository.java
@@ -1,6 +1,7 @@
 package com.driver.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,7 @@ import com.driver.model.Driver;
 @Repository
 public interface DriverRepository extends JpaRepository<Driver, Integer>{
 
+    Map<Object, Object> findById(int driverId);
+
+    void delete(Driver driver);
 }
